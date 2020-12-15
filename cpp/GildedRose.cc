565d49676725e72c2b4052394251ee6b44d043e8
Badplank <piotrek1879@gmail.com>
2020-12-15 14:51:40 +0100

    Deleted ragnaros name check

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -11,14 +11,11 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
         {
             if (items[i].quality > 0)
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
+                items[i].quality = items[i].quality - 1;
             }
         }
         else
