097b4754060f7d7ca6acd9493d1c464f2980207a
szaranczuk <igor.hanczaruk@gmail.com>
2021-01-18 21:14:32 +0100

    Removed some dumb ifs

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -34,17 +34,11 @@ void GildedRose::updateQuality()
                     {   
                         if (items[i].sellIn < 11)
                         {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            items[i].quality = items[i].quality + 1;
                         }
                         if (items[i].sellIn < 6)
                         {
-                            if (items[i].quality < 50)
-                            {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
